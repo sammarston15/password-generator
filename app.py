156fe71e2ba3a64263eb8411ac8fbc44e64f5708
Sam Marston <smarston@easypost.com>
2023-02-28 23:09:27 -0700

    finished password logic and styles

--- app.py
+++ app.py
@@ -1,12 +1,46 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from password_stuff import generate_password
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def index():
     return render_template('index.html')
+
+# handle form data
+@app.route('/submit', methods=['POST'])
+def submit():
+
+    if request.method == 'POST':
+        data = {}
+
+        # set password params from user input
+        data['length'] = request.form.get('length', None) # if user does not input number, the length value will be an empty string
+        data['uppercase'] = request.form.get('uppercase', None)
+        data['lowercase'] = request.form.get('lowercase', None)
+        data['numbers'] = request.form.get('numbers', None)
+        data['symbols'] = request.form.get('symbols', None)
+
+
+        try: 
+            # send password params to password generator
+            generated_password = generate_password(data=data)
+            print("generated password: ", generated_password)
+
+            # render the html template - NOTE: this will put you on the /submit page of the website when this template is rendered
+            return render_template('index.html', generated_password=generated_password) 
+
+            # you can also redirect to a new endpoint with the following
+            # return redirect('/', generated_password=generated_password)
+
+
+        except Exception as e:
+            print('hit error block')
+            print(f"{type(e)}: {e}")
+            # render the index.html template but with an alert at the top with the Exception
+            return render_template('index.html', error=f"Oops! There was a problem: {e}")
 
 
 if __name__ == "__main__":
-    # app.debug = True
+    app.debug = True
     app.run()
--- password_stuff.py
+++ password_stuff.py
@@ -1,127 +1,52 @@
-from email import message
-from tkinter import *
-from tkinter import messagebox
 from random import choice, randint, shuffle
-# import pyperclip
-import json
+import string
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
-#Password Generator Project
-def generate_password():
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-    password_letters = [choice(letters) for _ in range(randint(8, 10))]
-    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
-    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+def generate_password(data):
 
-    password_list = password_letters + password_symbols + password_numbers
-    shuffle(password_list)
+    password_list = []
+    password_uppercase = list(string.ascii_uppercase)
+    password_lowercase = list(string.ascii_lowercase)
+    password_numbers = list(string.digits)
+    password_symbols = list(string.punctuation)
 
+    # build password information based on user input
+    if data['uppercase'] == 'on':
+        password_list += password_uppercase
+    if data['lowercase'] == 'on':
+        password_list += password_lowercase
+    if data['numbers'] == 'on':
+        password_list += password_numbers
+    if data['symbols'] == 'on':
+        password_list += password_symbols
+    if data['length']:
+        # shuffle the password
+        shuffle(password_list)
+
+        # set the password length the user chose
+        password_list = password_list[:int(data['length'])]
+
+
+    # build default password information if no user input
+    if (data['length'] == '' and data['uppercase'] == None and data['lowercase'] == None 
+                                and data['numbers'] == None and data['symbols'] == None):
+        password_list = password_uppercase + password_lowercase + password_numbers + password_symbols
+
+        # shuffle the password
+        shuffle(password_list)
+
+        # set password length to 13 characters by default
+        password_list = password_list[:13]
+
+
+    # merge the password list back into a string
     password = "".join(password_list)
-    password_entry.insert(0, password)
-    # pyperclip.copy(password)
+    # print(password)
 
-
-# ---------------------------- SAVE PASSWORD ------------------------------- #
-def save():
-    website = website_entry.get()
-    email = email_entry.get()
-    password = password_entry.get()
-    new_data = {website: {
-        "email": email,
-        "password": password
-    }}
-
-    if len(website) == 0 or len(password) == 0:
-        messagebox.showinfo(title="oops!", message="Please make sure if you haven't left any fields blank.")
-    else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} \nPassword: {password} "
-                                                    f"\nIs it ok to save?")
-
-        if is_ok:
-            try:
-                with open("data.json", mode="r") as f:
-                    data = json.load(f) # reading the old data
-
-            except FileNotFoundError as error:
-                with open("data.json", mode="w") as f:
-                    json.dump(new_data, f, indent=4)
-
-            else:
-                data.update(new_data) # updating the data with new data
-                with open("data.json", mode="w") as f:
-                    json.dump(data, f, indent=4) # saving the new updated data
-
-            finally:
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
-
-
-# ---------------------------- Find Password ------------------------------- #
-def find_password():
-    website = website_entry.get()
-    try:
-        with open("data.json") as f:
-            data = json.load(f)
-    except FileNotFoundError:
-        messagebox.showinfo(title="Error", message="No data file found.")
-    else:
-        if website in data:
-            email = data[website]
-            password = data[website]["password"]
-            messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
-        else:
-            messagebox.showinfo(title="Error", message=f"No details for {website} exists.")
-
-# ---------------------------- UI SETUP ------------------------------- #
-window = Tk()
-window.title("Password Manager")
-window.config(padx=50, pady=50)
-
-canvas = Canvas(width=200, height=200, highlightthickness=0)
-logo_img = PhotoImage(file="logo.png")
-canvas.create_image(100, 100, image=logo_img)
-canvas.grid(column=1, row=0)
-
-# labels
-website_label = Label(text="Website:")
-website_label.grid(column=0, row=1)
-
-
-email_label = Label(text="Email/Username:")
-email_label.grid(column=0, row=2)
-
-password_label = Label(text="Password:")
-password_label.grid(column=0, row=3)
-
-# entries
-website_entry = Entry(width=21)
-website_entry.grid(column=1, row=1)
-website_entry.focus()
-
-email_entry = Entry(width=36)
-email_entry.grid(column=1, row=2, columnspan=2)
-email_entry.insert(0, "email@email.com")
-
-password_entry = Entry(width=21)
-password_entry.grid(column=1, row=3)
-
-# buttons
-search_button = Button(text="Search", width=13, command=find_password)
-search_button.grid(column=2, row=1)
-
-generate_password_button = Button(text="Generate Password", command=generate_password)
-generate_password_button.grid(column=2, row=3)
-
-add_button = Button(text="Add", width=36, command=save)
-add_button.grid(column=1, row=4, columnspan=2)
+    # return the password    
+    return password
 
 
 
-
-
-
-window.mainloop()
 
